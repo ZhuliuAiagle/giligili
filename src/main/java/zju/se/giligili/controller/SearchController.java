fc0a882be05ef8937abbde2a9bc8010fcecd3082
sanyangji <a1001_jason@163.com>
2019-07-14 09:42:33 +0800

    update Game.java

--- src/main/java/zju/se/giligili/controller/SearchController.java
+++ src/main/java/zju/se/giligili/controller/SearchController.java
@@ -2,7 +2,9 @@ package zju.se.giligili.controller;
 
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import zju.se.giligili.service.GameService;
 
@@ -11,8 +13,14 @@ import zju.se.giligili.service.GameService;
 public class SearchController {
     @Autowired
     private GameService gameService;
-    @RequestMapping("/s")
-    public String search(String key){
+
+    @RequestMapping("/games")
+    public String search(@RequestParam("key") String key){
             return "no result";
+    }
+
+    @RequestMapping("/game/{id}")
+    public String game(@PathVariable String id){
+        return null;
     }
 }
