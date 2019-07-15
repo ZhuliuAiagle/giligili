aba9bd64330ebb3ebeb0ac706685eb3b1e18fea8
ZhuliuAiagle <danielshen@yahoo.com>
2019-07-15 18:56:01 +0800

    Optimize code

--- src/main/java/zju/se/giligili/controller/SearchController.java
+++ src/main/java/zju/se/giligili/controller/SearchController.java
@@ -1,10 +1,8 @@
 package zju.se.giligili.controller;
 
 
-import com.kennycason.kumo.Word;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
